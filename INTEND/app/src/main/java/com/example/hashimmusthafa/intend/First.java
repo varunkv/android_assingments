76d094a18e27d9b3ae45ead3ce30f3370526b9ca
varunkv <varunkv666@gmail.com>
2016-10-05 13:31:54 +0530

    Added document

---
+++ INTEND/app/src/main/java/com/example/hashimmusthafa/intend/First.java
@@ -0,0 +1,65 @@
+package com.example.hashimmusthafa.intend;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class First extends AppCompatActivity {
+    Button set;
+    EditText number;
+    String string1="";
+    Intent i;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        getSupportActionBar().hide();
+
+        number=(EditText)findViewById(R.id.editText);
+
+
+
+        set=(Button)findViewById(R.id.button);
+        set.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 i=new Intent(First.this,SECOND.class);
+                string1=number.getText().toString();
+
+               i.putExtra("a",string1);
+                startActivity(i);
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_first, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ INTEND/app/src/main/java/com/example/hashimmusthafa/intend/SECOND.java
@@ -0,0 +1,50 @@
+package com.example.hashimmusthafa.intend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SECOND extends AppCompatActivity {
+    TextView content;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        getSupportActionBar().hide();
+
+        content=(TextView)findViewById(R.id.textView);
+       String a= getIntent().getStringExtra("a");
+
+        content.setText(a);
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_second, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
