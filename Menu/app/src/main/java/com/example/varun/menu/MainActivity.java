067c476fddb22c9c8ad7b1783ea9bd14af06d630
varunkv <varunkv666@gmail.com>
2016-10-05 13:22:24 +0530

    Added document

---
+++ Menu/app/src/main/java/com/example/varun/menu/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.varun.menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                Toast.makeText(this,"Settings is selected",Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_Bookmark:
+                Toast.makeText(this,"Bookmark is selected",Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_About:
+                Toast.makeText(this,"About Us",Toast.LENGTH_LONG).show();
+                return true;
+        }
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
